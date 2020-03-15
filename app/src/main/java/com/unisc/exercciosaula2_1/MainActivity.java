7dcc7819d2355643d5c0682de575afe488677fec
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-15 17:07:52 -0300

    Adiçã dos componentes no layout e mudança do ícone.

--- app/src/main/java/com/unisc/exercciosaula2_1/MainActivity.java
+++ app/src/main/java/com/unisc/exercciosaula2_1/MainActivity.java
@@ -3,12 +3,17 @@ package com.unisc.exercciosaula2_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageButton) findViewById(R.id.imagem);
     }
 }
